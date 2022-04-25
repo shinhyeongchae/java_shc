f669e9e6ab56013d0e168b904f292627bba944e8
shinhyeongchae <shin99123@nate.com>
2022-04-25 16:51:40 +0900

    자바 1일차 테스팅

---
+++ java/src/day1/Ex5_Test.java
@@ -0,0 +1,42 @@
+package day1;
+
+public class Ex5_Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* 과일가게에서 과일 판매량을 기록하려한다.
+		 * 사과, 오렌지 판매량을 기록하기 위한 변수는?
+		 * 
+		 * 정수타입
+		 * 
+		 * 
+		 */
+		int apple = 0;
+		int oragn = 0;
+		
+		/*
+		 *2-1학생 한명의 국어, 영어, 수학 성적을 저장하려 한다.
+		 *조건이 있냐 없냐에 따라서 달라진다 */
+
+		int KorScore = 0;
+		int engScore = 0;
+		int mathScore = 0;
+		
+	
+
+		/*2-2 . 학생의 학점을 출력하려고한다.
+		 * 이 때 학점을 저장하는 변수를 선언하세요.
+			학점은 0~4.5로 관리된다.
+			학점은 F~A 로 관리된다.
+			
+		 * */
+		
+		float score1 = 0.0f;
+		char score2 = 'F' ; 
+		
+		
+		
+		
+	}
+
+}
