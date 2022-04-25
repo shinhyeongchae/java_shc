c7bf1c47b1c81e10bc8d517e8396f2b99fa7e176
shinhyeongchae <shin99123@nate.com>
2022-04-25 13:57:22 +0900

    자바 1일차 : Hello world 출력 예제

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Ex1_Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world!!");
+		System.out.println("Hello world!!");
+		System.out.print("Hello world!!"); 
+	}
+	
+	
+}
+	
+	
+
+
+
