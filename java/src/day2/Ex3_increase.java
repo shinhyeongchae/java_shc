db9d444b97e938f79132d955c623678fa2745c0d
shinhyeongchae <shin99123@nate.com>
2022-04-27 08:37:50 +0900

    자바2일차 ex3

--- java/src/day2/Ex3_increase.java
+++ java/src/day2/Ex3_increase.java
@@ -7,19 +7,20 @@ public class Ex3_increase {
 		 *전위형 : ++변수명, 증가 후에 동작
 		 *후위형 : 변수명++, 동작 후에 증가*
 		 */
-	 int num1 = 10, num2 = 10;
+			int num1 = 10, num2 = 10;
 			//num1 : 전위형, num12 : 후위형
 			System.out.println(num1 + " , " + num2 );
 	 		System.out.println(++num1 + " , " + num2++ );
-	 		System.out.println(num1 + " , " + num2 );
+	 		System.out.println(num1 + " , " + num2);
 			
+	 		
 	 		int unm3 = 10, num4 = 10;
 	 		//num3 : 전위형, num4 : 후위형
-	 		System.out.println(num3 + " , " + num4);
-	 		num3++;
-	 		System.out.println(num3 + " , " + num4);
+	 		System.out.println(num4 + " , " + num4);
 	 		num4++;
-	 		System.out.println(num3 + " , " + num4);
+	 		System.out.println(num4 + " , " + num4);
+	 		num4++;
+	 		System.out.println(num4 + " , " + num4);
 	 		
 }
 
