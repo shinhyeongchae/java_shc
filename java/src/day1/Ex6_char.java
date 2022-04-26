ab9a5e7437567e4bd23f63b25d8c880a862c510b
shinhyeongchae <shin99123@nate.com>
2022-04-26 12:33:11 +0900

    자바 1일차

--- java/src/day1/Ex6_char.java
+++ java/src/day1/Ex6_char.java
@@ -12,16 +12,21 @@ public class Ex6_char {
 		char ch2 = '\n'; //엔터
 		char ch3 = '\r'; //커서를 앞으로 이동
 		char ch4 = '\"'; //"문자
-		char ch5 = '\\'; // 문자
-		char ch6 = '\\'; // /문자
+		char ch5 = '\''; //'문자 
+		char ch6 = '\\'; //\문자 
+		char ch7 = '\u0000'; //유니코드에 0에 해당하는 문자 
+		char ch8 = 'A';
+		
+		
 		
 		
 		
 		int num = 65;
 		char ch9 = (char)num;
-		System.out     .println(ch9);
+		System.out.println(ch9);
 		ch9 = (char)(num+1);
 		System.out.println(ch9);
 		
-}
-}
+}		
+		
+
