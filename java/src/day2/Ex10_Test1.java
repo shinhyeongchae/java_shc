f9bb38e5ce2e6115f17d8bee7cfdc4c1066dc065
shinhyeongchae <shin99123@nate.com>
2022-04-26 14:02:22 +0900

    자바2일차 연산자 연습문제;

---
+++ java/src/day2/Ex10_Test1.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Ex10_Test1 {
+
+	public static void main(String[] args) {
+		/*num가 짝수인지 아닌지 확인하는 코드를 작성하세요.
+		 * A성적 출력하는 예제를 활용
+		 * 짝수는 num를 2로 나누었을 때 나머지가 0과 같다.
+		 * num 2 % 0 ==
+		 * num % 2 ==0
+		 */
+		int num = 4;
+		System.out.println(num + "는 짝수인가?" + (num % 2 ==0));
+		
+				
+				
+	}
+
+}
