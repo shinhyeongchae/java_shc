f1f2b946fe197424a8ad190049ca1d1ccc0b53ed
shinhyeongchae <shin99123@nate.com>
2022-04-26 12:32:10 +0900

    자바2일차;비교연산자예제

---
+++ java/src/day2/Ex5_Comparison.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class Ex5_Comparison {
+
+	public static void main(String[] args) {
+		/*>= 반대로 쓰면 안됨 =>(x) / TODO Auto-generated method stub
+		 * <= 반대로 쓰면 안됨 <=(x)
+		 * >= 반대로 쓰면 안됨 =>(x)
+		 *==fmf =rhk 혼동하지말자 */
+		
+			int score = 95;
+			System.out.println(score > 95);
+			System.out.println(score >= 95);
+			System.out.println(score < 95);
+			System.out.println(score <= 95);
+			System.out.println(score == 95);
+			System.out.println(score != 95);
+			
+	
+	
+	}
+
+}
