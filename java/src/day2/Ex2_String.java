bc95d6d593356999484474d7de01129048c14020
shinhyeongchae <shin99123@nate.com>
2022-04-26 12:31:55 +0900

    자바2일차; 문자열및 문자열이어붙이기

---
+++ java/src/day2/Ex2_String.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Ex2_String {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		str = str + " world";
+		System.out.println(str);
+		str = str +	'!';
+		System.out.println(str);
+		str = str + 123;
+		System.out.println(str);
+		str = str + 3.45;
+		System.out.println(str);
+		str = str + true;
+		System.out.println(str);
+	}
+
+}
