d02bd0e23afa7ab214e62578f61aa57ceb4eec9a
shinhyeongchae <shin99123@nate.com>
2022-04-27 18:48:24 +0900

    자바3일차 If2

--- java/src/day3/Ex5_If2.java
+++ java/src/day3/Ex5_If2.java
@@ -14,12 +14,13 @@ public class Ex5_If2 {
 			
 		}else {
 			System.out.println(num + "는 6의 배수아님");
+			
 		}if(num % 2 ==0) {
 			if(num % 3 == 0);
 			System.out.println(num + "는 6의 배수");
 			
 		}else {
-			System.out.println(num + "는 6의 배수아님");
+			System.out.println(num + "는 2, 3, 6의 배수아님");
 		}
 
 	}
