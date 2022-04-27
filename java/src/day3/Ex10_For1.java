29bfa63e66b8e146677468e9e9e3474a99e145c3
shinhyeongchae <shin99123@nate.com>
2022-04-27 18:49:33 +0900

    자바3일차 For1

---
+++ java/src/day3/Ex10_For1.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Ex10_For1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		/*콘솔에 hello를 5번 출력하세요.*/
+		
+		int i; 
+		for(i=0;i<=5; i++) {
+			System.out.println("Hello");
+		}
+		
+	}
+
+}
