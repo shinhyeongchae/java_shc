a263dcfd9602669b9a9bb31c753bd8d032ecd221
shinhyeongchae <shin99123@nate.com>
2022-04-27 18:49:51 +0900

    자바3일차 For4

---
+++ java/src/day3/Ex13_For4.java
@@ -0,0 +1,29 @@
+package day3;
+
+public class Ex13_For4 {
+
+	public static void main(String[] args) {
+		/*1부터 5까지 합을 구하는 코드를 작성하세요*/
+		
+		
+		int i;
+		int sum = 0;
+		for(i =1; i<=5; i++) {
+			sum += i;
+		}
+			System.out.println("1부터 5까지 합 : " + sum);
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
