04ca5129a844cbd81b62ab41dce6882c027ff47d
shinhyeongchae <shin99123@nate.com>
2022-04-27 18:48:57 +0900

    자바3일차 Switch1

---
+++ java/src/day3/Ex7_Switch1.java
@@ -0,0 +1,26 @@
+package day3;
+
+public class Ex7_Switch1 {
+
+	public static void main(String[] args) {
+		/*홀수 짝수 판별 예제
+		 * num가 2로 나누었을 때 나머지가 0과 같으면 짝수라고 출력하고
+		 * 아니면 홀수라고 출력한다
+		 * */
+			int num = 3;
+			if(num % 2  == 0) {
+					System.out.println(num + "는 짝수");
+			}else {
+					System.out.println(num + "는 홀수");
+			}
+			
+			switch(num % 2) {
+			case 0 : 
+				System.out.println(num + "는 짝수");
+				break;
+			default :
+				 System.out.println(num + "는 홀수");
+			}
+
+}
+}
