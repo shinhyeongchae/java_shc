872443ce216863cee5beed5f158246c89999d726
shinhyeongchae <shin99123@nate.com>
2022-04-28 21:13:06 +0900

    자바 4일차 : 11

---
+++ java/src/day4/Ex11_While3.java
@@ -0,0 +1,24 @@
+package day4;
+
+public class Ex11_While3 {
+
+	public static void main(String[] args) {
+		/*a에서 z까지 출력하는 코드를 while문으로 작성하세요*/
+		//반복횟수 : i는 'a'부터 'z'까지 1씩 증가
+		//규칙성 : 	i를 출력
+		//반복문 종료 : 없음
+		
+		char i = 'a';
+		
+				while(i<='z') {
+			System.out.print(i);
+			i++;
+		}
+				char i1 = 'a';
+				
+				while(i<='z') {
+			System.out.print(i++);
+				}
+	}
+
+}
