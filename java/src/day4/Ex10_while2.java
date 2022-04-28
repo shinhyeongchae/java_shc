f47f8edd5b88cffb241241db04e9a3dbc8ac5169
shinhyeongchae <shin99123@nate.com>
2022-04-28 21:13:00 +0900

    자바 4일차 : 10

---
+++ java/src/day4/Ex10_while2.java
@@ -0,0 +1,34 @@
+package day4;
+
+public class Ex10_while2 {
+
+	public static void main(String[] args) {
+		/*1부터 5까지의 합을 구하는 코드를 while문을 이용하여 작성하시오
+		 * 
+		 * */
+	
+		int i =1, sum =0;
+		while(i<=5) {
+			sum = sum + i; 
+			i++;
+		}
+		System.out.println("1부터 5까지의 합 : " + sum );
+		i = 1;
+		sum = 0;
+	
+			
+			
+			
+			
+		
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
