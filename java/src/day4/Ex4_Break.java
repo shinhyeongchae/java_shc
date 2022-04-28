7dd118dd9507ca129cd54021954c5490b49f258f
shinhyeongchae <shin99123@nate.com>
2022-04-28 21:12:07 +0900

    자바 4일차 : 4

---
+++ java/src/day4/Ex4_Break.java
@@ -0,0 +1,18 @@
+package day4;
+
+public class Ex4_Break {
+
+	public static void main(String[] args) {
+		
+		for(int i= 1; i<=5; i++) {
+			System.out.println("hello");
+			if(i==3) {	
+			break;
+		 
+			}
+		}
+		
+		
+	}
+
+}
