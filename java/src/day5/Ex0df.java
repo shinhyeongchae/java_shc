a7580eaacf28b9d77e1263c736d73e0e5f00f541
shinhyeongchae <shin99123@nate.com>
2022-04-29 17:16:23 +0900

    자바 5일차 : 연습

---
+++ java/src/day5/Ex0df.java
@@ -0,0 +1,23 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Ex0df {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num = 2;
+		for(int i =2; i<=9;i++) {
+			 System.out.println(num + "x" + i + " = " + num*i );
+		}
+		
+		System.out.println();
+		scan.close();
+		
+	}
+
+
+}
