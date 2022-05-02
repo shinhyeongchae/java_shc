607b77fe510858693aa26313fdf03ad1a9b47fc6
shinhyeongchae <shin99123@nate.com>
2022-05-02 17:21:57 +0900

    자바 6일차 : Array1

---
+++ java/src/day6/Ex3_Array_Declaration.java
@@ -0,0 +1,30 @@
+package day6;
+
+public class Ex3_Array_Declaration {
+
+	public static void main(String[] args) {
+		int [] arr1;
+		int [] arr2 = null;
+		//5개짜리 정수 배열을 선언과 동시에 초기화
+		int [] arr3 = new int[5];
+		int size = 5;
+		//5개짜리 정수 배열을 선언과 동시에 초기화
+		int [] arr4 = new int[size];
+		//5개짜리 정수 배열을 선언과 동시에 초기화
+		int arr5[]= new int[5];
+	
+		int arr6[], arr7;//arr6 : 배열 - arr7:변수
+		int []arr8, arr9;//arr8 : 배열 - arr9:배열
+		//
+		int []arr10 = {1,2,3};
+		//
+		int []arr11 = new int[] {1,2,3};
+		//배명
+		
+		
+		
+	}
+	
+	
+
+}
