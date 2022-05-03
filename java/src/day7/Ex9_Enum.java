2b99a1dc368de8576d8430b257599f365990976d
shinhyeongchae <shin99123@nate.com>
2022-05-03 17:15:52 +0900

    자바 7일차 : 10_Enum2

---
+++ java/src/day7/Ex9_Enum.java
@@ -0,0 +1,34 @@
+package day7;
+
+import java.util.Calendar;
+
+public class Ex9_Enum {
+
+	public static void main(String[] args) {
+		/*오늘이 무슨 요일인지 확인하는 예제.  p.175*/
+			
+	
+		Calendar now = Calendar.getInstance();
+		int week = now.get(Calendar.DAY_OF_WEEK);
+		Week today = Week.MONDAY;
+		switch(week) {
+		case 1: today = Week.SUNDAY; break;
+		case 2: today = Week.MONDAY; break;
+		case 3: today = Week.TUESDAY; break;
+		case 4: today = Week.WEDNESDAY; break;
+		case 5: today = Week.THURSDAY; break;
+		case 6: today = Week.FRIDAY; break;
+		case 7: today = Week.SATURDAY; break;
+
+		}
+		System.out.println(today);
+		
+	}
+	
+	
+
+}
+
+enum Week{
+	MONDAY,TUESDAY,WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
