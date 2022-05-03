5a01645a08ddb9d2794fcf7ee70c5a9e3fe1d9fe
shinhyeongchae <shin99123@nate.com>
2022-05-03 17:15:29 +0900

    자바 7일차 : Multidimensional

---
+++ java/src/day7/Ex8_Array_Multidimensional.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class Ex8_Array_Multidimensional {
+
+	public static void main(String[] args) {
+		/*각 반에는 최대 30명
+		 * 1학년 1반의 국어 성적을 저장하기위한 배열은 선언해보세요*/
+		int score1[] = new int[30] ;
+		int score2[] = new int[30*10];
+		
+		score1[10] = 100;
+		score2[201] = 100;
+	}
+
+}
