2820b73c24df754059520ac0c873663da5c5159a
shinhyeongchae <shin99123@nate.com>
2022-05-10 17:15:04 +0900

    자바 11일차 : Ex4_fianl

---
+++ java/src/day11/Ex4_fianl.java
@@ -0,0 +1,28 @@
+package day11;
+
+public class Ex4_fianl {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Integer.MAX_VALUE);
+
+	}
+
+}
+class Tv{
+	private final static int MIN_CHANNEL = 1;
+	private int channel;
+	public void print() {
+		System.out.println("최소 채널 : " + MIN_CHANNEL);
+		System.out.println("현재 채널"+ channel);
+		
+	}
+	public Tv(int channel) {
+		this.channel = channel;
+		//minChannel = 2; //에러발생. final이 붙은 변수를 수정하려고 했기 때문에
+	}
+	
+	int a;
+	static int b;
+	final static int C=1;
+}
