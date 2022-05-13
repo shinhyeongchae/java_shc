603478e190f338a0fb183e35a462357b27e84537
shinhyeongchae <shin99123@nate.com>
2022-05-13 17:23:17 +0900

    13일차 : RadioController

---
+++ java/src/day13/RadioController.java
@@ -0,0 +1,41 @@
+package day13;
+
+public class RadioController implements Controller{
+	private double frequency;
+	private int volumn;
+	private boolean power;
+	@Override
+	public void run() {
+		power = true;
+		System.out.println("라디오 전원이 켜집니다.");
+		
+	}
+
+	@Override
+	public void stop() {
+		power = false;
+		System.out.println("라디오 전원이 꺼집니다.");
+		
+	}
+
+	@Override
+	public void print() {
+		System.out.println("전원 : " + (power?"켜짐":"꺼짐"));
+		System.out.println("주파수 : " + frequency);
+		System.out.println("음량 : " + volumn);
+	}
+	public void frequencyUp(){
+		frequency = power?frequency+0.1 : frequency;
+	}
+	public void frequencylDown() {
+		frequency = power?frequency-0.1 : frequency;
+	}
+	public void volumUp() {
+		volumn = power?volumn+1 : volumn;
+	}
+	public void volumDown() {
+		volumn = power?volumn-1 : volumn;
+	}
+}
+
+
