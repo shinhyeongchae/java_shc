499b15bb70c52f3fac3743acd3747209cf16879d
shinhyeongchae <shin99123@nate.com>
2022-05-13 16:08:01 +0900

    12일차 : SnackBox

---
+++ java/src/day12/SnackBox.java
@@ -0,0 +1,33 @@
+package day12;
+
+public class SnackBox extends Product {
+
+	
+	private int  count; 
+	public SnackBox(String name, int price,int amount, int count) {
+		super(name, price,amount);
+		this.count = count;
+		
+	}
+
+	public SnackBox(SnackBox product) {
+		this(product.getName(),product.getPrice()
+				,product.getAmount(),product.getCount());
+	}
+	@Override
+	public void print() {
+		System.out.println(super.getName()+"[박스당"+count+"개] : " +getAmount()+"원 - "
+				+getAmount());
+		
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	
+}
