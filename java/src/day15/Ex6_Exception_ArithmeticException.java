48847698617f88ffdb89285c8ff89a39e1ba1b15
shinhyeongchae <shin99123@nate.com>
2022-05-18 09:25:09 +0900

    자바 15일차 : Ex6_Exception_ArithmeticException

---
+++ java/src/day15/Ex6_Exception_ArithmeticException.java
@@ -0,0 +1,14 @@
+package day15;
+
+public class Ex6_Exception_ArithmeticException {
+
+	public static void main(String[] args) {
+		/*나누기나 / %를 이용할 때 발생할 수있다.*/
+		int num1 = 1, num2 = 0;
+		System.out.println(num1/num2);
+		
+		
+
+	}
+
+}
