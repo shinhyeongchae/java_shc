fd8d640b9ecad071e38c5bf69e36530f65abc5ac
shinhyeongchae <shin99123@nate.com>
2022-05-25 17:09:58 +0900

    Ex02_BoradMain

---
+++ java/src/day19/Ex02_BoradMain.java
@@ -0,0 +1,20 @@
+package day19;
+
+import java.util.Scanner;
+
+public class Ex02_BoradMain {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		BoardManager bm = new BoardManager(scan);
+		bm.run();//콘솔창에 실행하는 부분
+		
+		
+	
+			
+		
+	scan.close();
+	}
+}
+
