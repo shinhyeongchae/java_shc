4ce8c578541e38c8c21acc3828e40e90fe8012cd
shinhyeongchae <shin99123@nate.com>
2022-05-25 17:10:25 +0900

    Ex03_Thread2

---
+++ java/src/day20/Ex03_Thread2.java
@@ -0,0 +1,56 @@
+package day20;
+
+import java.awt.Toolkit;
+import java.net.Inet4Address;
+
+public class Ex03_Thread2 {
+
+	public static void main(String[] args) {
+			Thread th = Thread.currentThread();
+				Thread th1 = new Thread1();
+				Thread th2 = new Thread();
+				th1.setName("쓰레드1");
+				th1.setName("쓰레드2");
+				th1.start();
+				th2.start();
+				System.out.println("main 쓰레드 이름 : " + th.getName());
+				System.out.println("Thread1 이름 : " + th1.getName());
+				System.out.println("Thread2 이름 : " + th2.getName());
+				
+		}
+}
+
+
+
+class Thread1 extends Thread{
+	@Override
+	public void run() {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		
+		for(int i = 0; i<5; i++) {
+			tk.beep();
+			try {
+				Thread.sleep(1000);
+				
+			}catch(InterruptedException e) 		{}
+			
+		
+	} 
+}
+class Thread2 extends Thread{
+	@Override
+	public void run() {
+		for(int i = 0; i<5; i++) {
+			System.out.println("띵");
+			
+			
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e) {
+				
+			}
+		}
+		
+	}
+}
+}
