0cf700e6930de2689ebee911a57668bff4374cce
shinhyeongchae <shin99123@nate.com>
2022-06-08 17:17:44 +0900

    DB 6일차 : shoppingmall - 검색 예시

---
+++ DB/DB 6일차_02_shoppingmall_설치 예시.sql
@@ -0,0 +1,49 @@
+-- abc 회원의 누적 구매액
+select bu_us_id as 회원ID , sum(pr_price) as 누적금액 from buy 
+	join `option` on op_num = bu_op_num
+	join  product on pr_code = op_pr_code
+	where bu_us_id = 'abc123';
+-- 속도를 줄여주는 코드
+select bu_us_id as 회원ID , sum(pr_price) as 누적금액 
+	from (select * from buy where bu_us_id = 'abc123') as tmp
+	join `option` on op_num = bu_op_num
+	join  product on pr_code = op_pr_code;
+
+-- 각 제품별 구매 횟수를 조회(동일 회원 구매도 포함)
+select op_pr_code as 제품, count(bu_num) as 구매횟수 from buy 
+	right join `option` on bu_op_num = op_num
+	group by op_pr_code;
+    
+-- 모든 제품에 대해 각 제품별, 옵션별 구매 횟수를 조회(동일 회원 구매도 포함)
+select op_pr_code as 제품코드 , op_size as 옵션 , count(bu_num) as 구매횟수 
+	from `option`
+	left join buy on op_num = bu_op_num
+    group by op_num;
+
+-- 제품코드가 ABC001인 제품 정보를 조회(제품코드, 제품명, 제품분류, 가격, 구매가능한 사이즈)
+select * from product
+	join `option` on op_pr_code = pr_code
+	where op_pr_code = 'ABC001' and op_amount > 0;
+
+-- abc123 회원이 구매한 제품명 조회(중복x)
+select pr_title as 제품명 from buy 
+	join `option` on op_num = bu_op_num
+	join  product on pr_code = op_pr_code
+	where bu_us_id = 'abc123'
+    group by pr_code;  -- 제품명이 같을경우 distinct보다는 group by를 통하는게 좋다.
+
+-- 2000년생 회원들의 구매 제품 목록을 조회
+select distinct op_pr_code from buy 
+	join `option` on bu_op_num = op_num
+    join user on bu_us_id = us_id
+    where us_birth like '2000%';
+    
+select distinct op_pr_code from buy 
+	join `option` on op_num = bu_op_num
+	join  user on bu_us_id = us_id
+	where us_birth between '2000-01-01' and '2000-12-31';
+   
+    
+    
+    
+    
