874808a4977e89ef2a75a7359835663204c7acf1
shinhyeongchae <shin99123@nate.com>
2022-06-20 15:02:30 +0900

    ank).sql DB 14일차 : 윈도우함수(row_number, rank, dense_rank)

---
+++ DB/DB 14일차_07_윈도우함수(row_number, rank, dense_rank).sql
@@ -0,0 +1,39 @@
+/*
+row_number() : 순서를 세서 1부터 차례대로 숫자를 할당, over() 안에 있는 것을 기준으로,
+순서라는 속성을 하나 만들고 (실제 테이블에 속성을 추가하는 것이 아니라 결과화면에 추가), 그 속성값으로 1부터 
+하나씩 증가하도록 하는데 기준을 over()안에 있는 정렬방법 기준으로 삼음.
+같은 값이 있으면 번호가 다름 
+2 < 12
+'2' > '12'
+ */
+-- row_number() 게시물 번호가 아닌 일반 번호로 바꿔주는 역할.
+-- row_number() 순서를 세는데 over()  ()안에에 있는것을 기준으로 순서를 매긴다.
+-- 일반 게시글을 등록 순서대로 정렬.
+SELECT 
+    row_number() over(order by bd_num)순서, bd_num, bd_title  
+FROM
+    community.board
+WHERE
+    bd_ca_name = '일반' ;
+-- order by bd_num desc ; -- 여기서 order by bd_num desc;는 제일 최신에 올린 게시물을 바탕으로 순서를 매겨준다.
+
+/*
+rank() 는 row_number() 와 다르게 값이 같으면 같은 값을 가지고, 다음 번호는 생략
+dense_rank()는 rank()와 다르게 다음 번호를 생략하지 않음*/
+-- 일반 게시글들을 비추천 순으로 정렬
+SELECT 
+    rank() over(order by bd_down desc)순서, board.*
+FROM
+    community.board
+WHERE
+    bd_ca_name = '일반' 
+order by bd_down desc, 순서 asc ;
+
+SELECT 
+    dense_rank() over(order by bd_down desc)순서, board.*
+FROM
+    community.board
+WHERE
+    bd_ca_name = '일반' 
+order by bd_down desc, 순서 asc ;
+    
