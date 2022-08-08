10dcc7364c3e37a18a7e94fab30d5673e770704e
shinhyeongchae <shin99123@nate.com>
2022-08-08 12:05:33 +0900

    spring 복습 : DB 연동 완료

--- spring_shc_test/src/main/java/kr/green/springtest/controller/HomeController.java
+++ spring_shc_test/src/main/java/kr/green/springtest/controller/HomeController.java
@@ -1,20 +1,27 @@
-package kr.green.springtest;
+package kr.green.springtest.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.springtest.service.MemberService;
 
 @Controller
 public class HomeController {
 	
 		private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+		@Autowired
+		MemberService memberService;
 		
 		@RequestMapping(value= {"/"})
 		public ModelAndView openTilesView(ModelAndView mv) throws Exception{
 		    mv.setViewName("/main/home");
 		    mv.addObject("setHeader", "타일즈");
+		    System.out.println(memberService.getEmail("qwe"));
 		    return mv;
 		}
 }
+
