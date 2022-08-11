9c140af8d8c6af6882b91f650b32296f8722c08b
shinhyeongchae <shin99123@nate.com>
2022-08-11 17:13:08 +0900

    spring : 게시글 추천/비추천 기능 구현 완료(선택된 추천/비추천 색상 미적용)

--- spring_shc/src/main/java/kr/green/spring/controller/BoardController.java
+++ spring_shc/src/main/java/kr/green/spring/controller/BoardController.java
@@ -7,97 +7,105 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.green.spring.pagination.Criteria;
 import kr.green.spring.pagination.PageMaker;
 import kr.green.spring.service.BoardService;
 import kr.green.spring.vo.BoardVO;
+import kr.green.spring.vo.LikesVO;
 import kr.green.spring.vo.MemberVO;
 
 @Controller
 public class BoardController {
+	
 	@Autowired
 	BoardService boardService;
-	//url 등록
+	
 	@RequestMapping(value="/board/list", method=RequestMethod.GET)
-	public ModelAndView boardListGet(ModelAndView mv, Criteria cri) {
-			cri.setPerPageNum(2);
-			System.out.println(cri);
-			int totalCount = boardService.getTotalCount(cri);
-			
-			//등록된 게시글을 가져옴(여러개)
-			ArrayList<BoardVO> list = boardService.getBoardList(cri);
-			PageMaker pm = new PageMaker(cri, 5, totalCount);
-			System.out.println(pm);
-			mv.addObject("pm",pm);
-			mv.addObject("list",list);
-	    mv.setViewName("/board/list");
-	    return mv;
+	public ModelAndView boardListGet(ModelAndView mv, Criteria cri){
+		cri.setPerPageNum(2);
+		int totalCount = boardService.getTotalCount(cri);
+		//등록된 게시글을 가져옴(여러개)
+		ArrayList<BoardVO> list = boardService.getBoardList(cri);
+		PageMaker pm = new PageMaker(cri, 5, totalCount);
+		mv.addObject("pm", pm);
+		mv.addObject("list", list);
+    mv.setViewName("/board/list");
+    return mv;
 	}
 	
 	@RequestMapping(value="/board/insert", method=RequestMethod.GET)
-	public ModelAndView boardInsert(ModelAndView mv) {
-	    mv.setViewName("/board/insert");
-	    return mv;
+	public ModelAndView boardInsertGet(ModelAndView mv){
+    mv.setViewName("/board/insert");
+    return mv;
 	}
+	
 	@RequestMapping(value="/board/insert", method=RequestMethod.POST)
-	public ModelAndView boardInsertPost(ModelAndView mv, BoardVO board,
-			HttpSession seesion) {
-		 	//화면에서 전송한 데이터가 잘 오는지 확인
-			//System.out.println(board); 
-			//로그인한 회원 정보를 확인
-			MemberVO user = (MemberVO)seesion.getAttribute("user");
-			//System.out.println(user);
-			boardService.insertBoard(board,user);
-	    mv.setViewName("redirect:/board/list");
-	    return mv;
+	public ModelAndView boardInsertPost(ModelAndView mv, BoardVO board, 
+			HttpSession session){
+		//화면에서 전송한 데이터가 잘 오는지 확인
+		//System.out.println(board);
+		//로그인한 회원 정보를 확인
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		//System.out.println(user);
+		boardService.insertBoard(board,user);
+    mv.setViewName("redirect:/board/list");
+    return mv;
 	}
+	
 	@RequestMapping(value="/board/select/{bd_num}", method=RequestMethod.GET)
-	public ModelAndView boardselectGet(ModelAndView mv, @PathVariable("bd_num")Integer bd_num) {
-			//게시글 번호에 맞는 게시글 조회수를 증가
-			boardService.updateViews(bd_num);
-			//게시글 번호에 맞는 게시글 정보를 가져옴
-			BoardVO board = boardService.getBoard(bd_num);
-			System.out.println(board);
-			//가져온 게시글을 화면에 전달
-			mv.addObject("board", board);
-	    mv.setViewName("/board/select");
-	    return mv;
-	}	
-	@RequestMapping(value="/board/update/{bd_num}", method=RequestMethod.GET)
-	public ModelAndView boardUpdateGet(ModelAndView mv, @PathVariable("bd_num")Integer bd_num) {
+	public ModelAndView boardSelectGet(ModelAndView mv, @PathVariable("bd_num")Integer bd_num){
+		//게시글 번호에 맞는 게시글 조회수를 증가
+		boardService.updateViews(bd_num);
 		//게시글 번호에 맞는 게시글 정보를 가져옴
 		BoardVO board = boardService.getBoard(bd_num);
-		System.out.println(board);
+		//가져온 게시글을 화면에 전달
+		mv.addObject("board", board);
+    mv.setViewName("/board/select");
+    return mv;
+	}
+	@RequestMapping(value="/board/update/{bd_num}", method=RequestMethod.GET)
+	public ModelAndView boardUpdateGet(ModelAndView mv, @PathVariable("bd_num")Integer bd_num){
+		//게시글 번호에 맞는 게시글 정보를 가져옴
+		BoardVO board = boardService.getBoard(bd_num);
 		//가져온 게시글을 화면에 전달
 		mv.addObject("board", board);
     mv.setViewName("/board/update");
     return mv;
 	}
+	
 	@RequestMapping(value="/board/update/{bd_num}", method=RequestMethod.POST)
-	public ModelAndView boardUpdatePost(ModelAndView mv,
-			@PathVariable("bd_num")Integer bd_num, HttpSession session, BoardVO board) {
+	public ModelAndView boardUpdatePost(ModelAndView mv, 
+			@PathVariable("bd_num")Integer bd_num, HttpSession session, BoardVO board){
 		//수정한 게시글 정보를 확인
 		//System.out.println(board);
 		//로그인한 회원 정보를 확인
 		MemberVO user = (MemberVO) session.getAttribute("user");
-		System.out.println(user);
+		//System.out.println(user);
 		//게시글 수정 요청
 		board.setBd_num(bd_num);
 		boardService.updateBoard(board,user);
     mv.setViewName("redirect:/board/select/"+bd_num);
     return mv;
 	}
+	
 	@RequestMapping(value="/board/delete/{bd_num}", method=RequestMethod.GET)
 	public ModelAndView boardDeleteGet(ModelAndView mv, 
-			@PathVariable("bd_num")Integer bd_num, HttpSession session) {
+			@PathVariable("bd_num")Integer bd_num, HttpSession session){
 		MemberVO user = (MemberVO) session.getAttribute("user");
 		boardService.deleteBoard(bd_num, user);
 		
     mv.setViewName("redirect:/board/list");
     return mv;
+	}
+	@RequestMapping(value="/board/likes", method=RequestMethod.POST)
+	@ResponseBody
+	public String boardLikes(@RequestBody LikesVO likes){
+		return boardService.updateLikes(likes);
 	}
 }
--- spring_shc/src/main/webapp/WEB-INF/views/board/select.jsp
+++ spring_shc/src/main/webapp/WEB-INF/views/board/select.jsp
@@ -1,6 +1,6 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>  
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,13 +9,13 @@
 </head>
 <body>
 	<div class="container">
-		<c:if test="${board != null && 'N'.charAt(0) == board.bd_del }">
+		<c:if test="${board != null && 'N'.charAt(0) ==board.bd_del }">
 			<h1>게시글 상세</h1>
 			<div class="form-group">
-			  <input type="text" class="form-control" name="bd_title" value="${board.bd_title}" readonly>
+			  <input type="text" class="form-control" name="bd_title" value="${board.bd_title }" readonly>
 			</div>
 			<div class="form-group">
-			  <input type="text" class="form-control" name="bd_me_id" value="${board.bd_me_id}" readonly>
+			  <input type="text" class="form-control" name="bd_me_id" value="${board.bd_me_id }" readonly>
 			</div>
 			<div class="form-group">
 			  <input type="text" class="form-control" name="bd_reg_date" value="${board.bd_reg_date_time_str}" readonly>
@@ -24,23 +24,64 @@
 			  <input type="text" class="form-control" name="bd_views" value="${board.bd_views}" readonly>
 			</div>
 			<div class="form-group">
-			  <textarea class="form-control" rows="10"  name="bd_content" readonly>${board.bd_content}</textarea>
+				<button type="button" class="btn btn-outline-primary up btn-likes" >추천</button>
+				<button type="button" class="btn btn-outline-danger down btn-likes">비추천</button>
 			</div>
-			<c:if test="${user != null && user.me_id == board.bd_me_id}">
+			<div class="form-group">
+			  <textarea class="form-control" rows="10" name="bd_content" readonly>${board.bd_content}</textarea>
+			</div>
+			<c:if test="${user != null && user.me_id == board.bd_me_id }">
 				<a href="<%=request.getContextPath()%>/board/update/${board.bd_num}" class="btn btn-outline-danger">수정</a>
 				<a href="<%=request.getContextPath()%>/board/delete/${board.bd_num}" class="btn btn-outline-danger">삭제</a>
 			</c:if>
 		</c:if>
-		<c:if test="${board != null && 'A'.charAt(0) == board.bd_del }">
-			<h1>관리자에 의해 삭제된 게시글입니다</h1>
+		<c:if test="${board != null && 'A'.charAt(0) ==board.bd_del }">
+			<h1>관리자에 의해 삭제된 게시글입니다.</h1>
 		</c:if>
-		<c:if test="${board != null && 'Y'.charAt(0) == board.bd_del }">
-			<h1>작성자에 의해 삭제된 게시글입니다</h1>
+		<c:if test="${board != null && 'Y'.charAt(0) ==board.bd_del }">
+			<h1>작성자에 의해 삭제된 게시글입니다.</h1>
 		</c:if>
-		<c:if test="${board = null}">
+		<c:if test="${board == null}">
 			<h1>잘못된 경로로 접근했습니다.</h1>
-		</c:if>	
+		</c:if>
 	</div>
-	${board}
+	<script>
+		$(function(){
+			$('.btn-likes').click(function(){
+				let li_state = $(this).hasClass('up') ? 1 : -1;
+				let obj = {
+					li_bd_num : '${board.bd_num}',
+					li_state : li_state,
+					li_me_id : '${user.me_id}'
+				}
+				if(obj.li_me_id == ''){
+					if(confirm('추천/비추천은 로그인을 해야 합니다. 로그인을 하시겠습니까?')){
+						location.href='<%=request.getContextPath()%>/login'
+					}
+					else
+						return;
+				}
+				$.ajax({
+	        async:true,
+	        type:'POST',
+	        data: JSON.stringify(obj),
+	        url: '<%=request.getContextPath()%>/board/likes',
+	        contentType:"application/json; charset=UTF-8",
+	        success : function(data){
+	        	if(data == '1')
+	        		alert('해당 게시글을 추천했습니다.')
+	        	else if(data == '-1')
+	        		alert('해당 게시글을 비추천했습니다.')
+	        	else if(data == '10')
+	        		alert('해당 게시글 추천을 취소했습니다.')
+	        	else if(data == '-10')
+	        		alert('해당 게시글 비추천을 취소했습니다.')
+	        	else
+	        		alert('잘못된 접근입니다.');
+	        }
+		    });
+			})
+		})
+	</script>
 </body>
-</html> 
+</html>
